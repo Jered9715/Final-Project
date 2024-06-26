d61fdc4fefefda0aaaaa9b01e3163682158414ba
Jess Gates <gatespj@outlook.com>
2024-06-25 21:38:42 -0400

    added national parks API initial data, we can refine what we want later

--- Final-Project/src/app/services/national-park.service.ts
+++ Final-Project/src/app/services/national-park.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ParkResponse } from '../interfaces/park';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NationalParkService {
+  private apiUrl = 'developer.nps.gov/api/v1';
+  private apiKey = 'r4OSxniaCtevFyb5dCkZc0z0D8bSQLsTRdfK7o9L';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getParks(): Observable<ParkResponse> {
+    const headers = new HttpHeaders({
+      'X-Api-Key': this.apiKey
+    });
+    return this.http.get<ParkResponse>(`${this.apiUrl}/parks`, {headers: headers});
+  }
 }
+

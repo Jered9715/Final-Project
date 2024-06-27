0cf2fb65a62bc440d3d83bd4d9691d9c329451f7
kmundie2 <mundiek@charter.net>
2024-06-26 20:49:21 -0400

    updated with working Google Maps API

--- Final-Project/src/app/app.routes.ts
+++ Final-Project/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MapsComponent } from './components/maps/maps.component';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/park-list', pathMatch: 'full'}
+];
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideRouter(routes),
+        importProvidersFrom(HttpClientModule)
+    ]
+});

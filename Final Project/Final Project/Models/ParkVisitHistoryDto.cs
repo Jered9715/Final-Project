736cf2c744c2f2cc36931b44ca0fdba9f5667611
Christie018 <christie.sullivan018@gmail.com>
2024-07-01 21:30:26 -0400

    Adding DTOs and updating connection string

---
+++ Final Project/Final Project/Models/ParkVisitHistoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_Project.Models
+{
+    public class ParkVisitHistoryDto
+    {
+        public int ParkVisitId { get; set; }
+        public int UserId { get; set; }
+        public string ParkNotes { get; set; }
+        public string DateVisited { get; set; }
+    }
+}

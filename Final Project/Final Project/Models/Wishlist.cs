1539e212f991c13c0daaa1da2865eaeeafdef313
Christie018 <christie.sullivan018@gmail.com>
2024-07-01 21:08:41 -0400

    Adding database to API

---
+++ Final Project/Final Project/Models/Wishlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Final_Project.Models;
+
+public partial class Wishlist
+{
+    public int WishListId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? ParkId { get; set; }
+
+    public virtual User? User { get; set; }
+}

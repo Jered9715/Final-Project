73c1e2f849ba9b489683567b3dc294ce8c39b8f1
Christie018 <christie.sullivan018@gmail.com>
2024-07-06 01:33:15 -0400

    Finishing API w/ Wishlist Controller

--- Final Project/Final Project/Controllers/WishlistController.cs
+++ Final Project/Final Project/Controllers/WishlistController.cs
@@ -1,6 +1,59 @@
-﻿namespace Final_Project.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Final_Project.DAL;
+using Final_Project.Models;
+
+namespace Final_Project.Controllers
 {
-    public class WishlistController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
     {
+        private readonly WishlistRepository _wishlistRepository;
+        public WishlistController(WishlistRepository wishlistRepository)
+        {
+            _wishlistRepository = wishlistRepository;
+        }
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<IEnumerable<WishlistDto>>>
+
+        GetWishlists(int userId)
+
+        {
+            var wishlists = await
+
+            _wishlistRepository.GetAllWishlistsAsync(userId);
+
+            return Ok(wishlists);
+        }
+        [HttpPost]
+        public async Task<ActionResult<WishlistDto>>
+
+        PostWishlist(WishlistDto wishlistDto)
+
+        {
+            var createdWishlist = await
+
+            _wishlistRepository.AddWishlistAsync(wishlistDto);
+
+            return CreatedAtAction(nameof(GetWishlists), new
+            {
+                userId = createdWishlist.UserId
+            }, createdWishlist);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWishlist(int id)
+
+        {
+            var deleted = await
+
+            _wishlistRepository.DeleteWishlistAsync(id);
+
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
--- Final Project/Final Project/Program.cs
+++ Final Project/Final Project/Program.cs
@@ -15,7 +15,21 @@ builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<ParkVisitHistoryRepository>();
 builder.Services.AddScoped<WishlistRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins"); // Use the CORS policy
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

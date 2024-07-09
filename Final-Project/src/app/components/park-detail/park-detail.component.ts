aae2b8bdcb3f3859637ece3db59dc9d3bfcc6437
Jered O'Grady <jered9715@gmail.com>
2024-07-08 21:45:24 -0400

    added expansion cards to park detail component

--- Final-Project/src/app/components/park-detail/park-detail.component.ts
+++ Final-Project/src/app/components/park-detail/park-detail.component.ts
@@ -9,12 +9,13 @@ import { ParkResponse, Park } from '../../interfaces/park';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap} from 'rxjs';
 import { map } from 'rxjs';
-
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-park-detail',
   standalone: true,
-  imports: [RouterModule, FormsModule, CommonModule, HttpClientModule, MapsComponent],
+  imports: [RouterModule, FormsModule, CommonModule, HttpClientModule, MapsComponent,MatListModule,MatExpansionModule],
   providers: [NationalParkService],
   templateUrl: './park-detail.component.html',
   styleUrl: './park-detail.component.scss'
@@ -27,7 +28,7 @@ export class ParkDetailComponent implements OnInit {
   long= ''
   lat= ''
   mapUrl = ''
-
+  panelOpenState = false;
   constructor(private route: ActivatedRoute, private nationalParkService: NationalParkService) { }
 
   ngOnInit(): void {

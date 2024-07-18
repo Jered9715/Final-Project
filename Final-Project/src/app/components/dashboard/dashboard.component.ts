756cba3ff3bb770c8fdf58c0f7eab953bbd2f35e
Christie018 <christie.sullivan018@gmail.com>
2024-07-17 20:26:51 -0400

    Adding progress bar

--- Final-Project/src/app/components/dashboard/dashboard.component.ts
+++ Final-Project/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ParkVisitHistoryComponent } from '../park-visit-history/park-visit-history.component';
 import { WishlistComponent } from '../wishlist/wishlist.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ParkVisitHistoryComponent, WishlistComponent],
+  imports: [ParkVisitHistoryComponent, WishlistComponent, MatProgressBarModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
